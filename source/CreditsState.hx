bee6294d899d6584855dc1e767b57d694397bb51
4techguns <4techgunsinbox@gmail.com>
2021-12-23 21:26:01 -0330

    ALPHA 7.1 - add yuiuo to credits - fix the noky - give the noky an icon

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -80,11 +80,12 @@ class CreditsState extends MusicBeatState
 
 		var pisspoop:Array<Array<String>> = [ //Name - Icon name - Description - Link - BG Color
 			['vs noky'],
-			['Ragarnoky',		'thenoky',		'star of the show',						'https://www.youtube.com/channel/UC3Y_eevUSaVNroiEPJubO0Q',	'00aa00'],
-			['brohmon',		'broh',		'programmer, composer and owner',						'https://www.youtube.com/channel/UCbJWOoN78xpD5qWVSUIXGkQ',	'00ffff'],
+			['Ragarnoky',		'thenoky',		'THE NOKY - composer and maker of 10% of the dialogue',						'https://www.youtube.com/channel/UC3Y_eevUSaVNroiEPJubO0Q',	'00aa00'],
+			['brohmon',		'broh',		'artist, programmer, charter',						'https://www.youtube.com/channel/UCbJWOoN78xpD5qWVSUIXGkQ',	'00ffff'],
+			['yuiuo',		'yuiuo',		'made zak\'s icons',						'https://www.youtube.com/channel/UC1zndvuymlj0msFJ8oXoqnQ',	'00ffff'],
 			['balls song'],
 			['Ragarnoky',		'thenoky',		'balls',						'https://www.youtube.com/channel/UC3Y_eevUSaVNroiEPJubO0Q',	'00aa00'],
-			['Indigo',		'thenoky',		'balls',						'https://www.youtube.com/channel/UCJPlfnqSCPHEVJDzgagtxXg',	'000099'],
+			['Indigo',		'thenoky',		'they\'re inside your walls',						'https://www.youtube.com/channel/UCJPlfnqSCPHEVJDzgagtxXg',	'000099'],
 			['Vic',		'thenoky',		'vic vic vic vic vic vic vic vic vic vic vic',						'https://www.youtube.com/channel/UCol8veOHkFoxGda98Eow_dw',	'00ffff'],
 			['balls'],
 			['balls',		'balls',		'BALLS',						'https://www.youtube.com/watch?v=btuymnVDU28',	'00aa00'],
